2a30807c4e9ad97cbf7540e9487e3154d8c99f2c
tecsup <tecsup@gmail.com>
2019-04-01 17:47:58 -0500

    it added firebase

---
+++ dist/assets/scripts/firebaseKey.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAdtTmQaA54G6SFtiiSqTjyYytld2iNGno",
+  authDomain: "coderecruiter-a4ce8.firebaseapp.com",
+  databaseURL: "https://coderecruiter-a4ce8.firebaseio.com",
+  projectId: "coderecruiter-a4ce8",
+  storageBucket: "coderecruiter-a4ce8.appspot.com",
+  messagingSenderId: "868741664185"
+};
+firebase.initializeApp(config);
